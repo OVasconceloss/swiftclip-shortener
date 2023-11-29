ec825e2191ac328e5fac21b9249e2ac110a6192e
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2023-11-29 09:12:01 +0000

    feat: Create route system file

---
+++ src/routes/Router.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export const Router = () => {
+    <BrowserRouter>
+        <Routes>
+            <Route path="" element="" />
+        </Routes>  
+    </BrowserRouter>
+};
