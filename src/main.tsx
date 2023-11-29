225deda5fdbafed6a03e146ccbbb0fae566e01d1
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2023-11-29 09:25:54 +0000

    feat: Import route file

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import Router from './routes/Router';
 import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')!).render();
+ReactDOM.createRoot(document.getElementById('root')!).render(<Router />);
