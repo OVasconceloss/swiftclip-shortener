227ec15e3ee95e97cb5828973850622ca6655611
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2023-11-30 08:41:32 +0000

    chore: Create the main elements of the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
 export const Header = () => {
     return (
         <header className="header">
-
+            <h1 className="header-title">Switftclip</h1>
+            <nav className="header-nav">
+                <Link to={`/`}>
+                    <a href="/" className="nav-link">Home</a>
+                </Link>
+                <Link to={`/about`}>
+                    <a href="#" className="nav-link">About</a>
+                </Link>
+                <a href="#" className="nav-link">
+                    <button className="nav-button">
+                        <i className="fa-brands fa-github"></i>
+                    </button>
+                </a>
+            </nav>
         </header>
     );
 };
