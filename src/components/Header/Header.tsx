cd3b8229b6743defefd5a801cf409d4dbd3ae4cb
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2023-11-30 09:36:59 +0000

    feat: Add elements and classes for these elements

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import "./Header.scss";
 import { Link } from "react-router-dom";
 
 export const Header = () => {
@@ -5,13 +6,17 @@ export const Header = () => {
         <header className="header">
             <h1 className="header-title">Switftclip</h1>
             <nav className="header-nav">
-                <Link to={`/`}>
+                <Link to={`/`} className="link-header">
                     <a href="/" className="nav-link">Home</a>
                 </Link>
-                <Link to={`/about`}>
+                <Link to={`/about`} className="link-header">
                     <a href="#" className="nav-link">About</a>
                 </Link>
-                <a href="#" className="nav-link">
+                <a 
+                    href="https://github.com/OVasconceloss/swiftclip-shortener" 
+                    target={`_blank`} 
+                    className="nav-link"
+                >
                     <button className="nav-button">
                         <i className="fa-brands fa-github"></i>
                     </button>
