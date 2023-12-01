8fa6a5a46ca409a2fddd7aa5dcbec2264dad051b
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2023-12-01 16:52:59 +0000

    chore: Small change in the text of one of the links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ export const Header: React.FC = () => {
             <h1 className="header-title">Switftclip</h1>
             <nav className="header-nav">
                 <Link to={`/`} className="link-header nav-link">Home</Link>
-                <Link to={`/about`} className="link-header nav-link">Abut</Link>
+                <Link to={`/about`} className="link-header nav-link">About</Link>
                 <a href="https://github.com/OVasconceloss/swiftclip-shortener" target={`_blank`} className="nav-link">
                     <button className="nav-button">
                         <i className="fa-brands fa-github"></i>
