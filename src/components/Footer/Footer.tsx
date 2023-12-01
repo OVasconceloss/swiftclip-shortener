65765c3e71e1943aca66e095069945b62f3d55ad
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2023-12-01 09:56:49 +0000

    feat: Create Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,37 @@
-export const Footer = () => {
+import "./Footer.scss";
+import { Link } from "react-router-dom";
+
+export const Footer: React.FC = () => {
     return (
         <footer className="footer">
+            <h2 className="footer-title">Switftclip</h2>
+            <p className="footer-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Praesentium eum, nam inventore accusamus eaque magnam saepe, aperiam
+                rerum quis non omnis minus accusantium rem voluptas? Rerum reprehenderit laudantium commodi deleniti.
+            </p>
+            <div className="footer-contacts">
 
+            </div>
+            <div className="footer-copyright">
+                <h3 className="copyright-text">Copyright &copy;2023 Victor Vasconcelos</h3>
+                <div className="copyright-menu">
+                    <Link to={`/`} className="link-footer">
+                        <a href="/" className="menu-footer-link">Home</a>
+                    </Link>
+                    <Link to={`/about`} className="link-footer">
+                        <a href="#" className="menu-footer-link">About</a>
+                    </Link>
+                    <a 
+                        href="https://github.com/OVasconceloss/swiftclip-shortener" 
+                        target={`_blank`} 
+                        className="menu-footer-link">
+                        <button className="menu-footer-button">
+                            <i className="fa-brands fa-github"></i>
+                        </button>
+                    </a>
+                </div>
+            </div>
         </footer>
     );
 };
