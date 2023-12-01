ab45e3e1e3947fcb306900482a1401ae71285f0f
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2023-12-01 12:10:11 +0000

    feat: Create main elements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import "../styles/home.scss";
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
 
@@ -5,6 +6,18 @@ const Home = () => {
     return (
         <>
         <Header />
+        <main className="main">
+            <section className="main-section-url">
+                <div className="section-url-text">
+                    <h1 className="section-url-title">Free URL Shortener</h1>
+                    <h2 className="section-url-subtitle">Minimize Links, Maximize Convenience: Your Ultimate URL Shortening Solution</h2>
+                </div>
+                <div className="section-url-input">
+                    <input type="url" name="url" id="url" className="url-input" placeholder="Ex: https://www.youtube.com/watch?v=b9eMGE7QtTk"/>
+                    <button className="url-input-button">Shorten URL</button>
+                </div>
+            </section>
+        </main>
         <Footer />
         </>
     );
