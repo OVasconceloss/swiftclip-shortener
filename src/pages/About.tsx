ad5783b1115bd297e1f28aec29db3d8fc4f4062d
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2023-12-04 08:27:40 +0000

    feat: Import Header and Footer component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,11 @@
+import { Footer } from "../components/Footer/Footer";
+import { Header } from "../components/Header/Header";
+
 const About: React.FC = () => {
     return (
         <>
-        
+        <Header />
+        <Footer />
         </>
     );
 };
