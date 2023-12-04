ceb65d7a25557618b04f9df174b7441b504b48df
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2023-12-04 08:25:53 +0000

    feat: Import and create route for About page

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,10 +1,12 @@
 import Home from "../pages/Home";
+import About from "../pages/About";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Router: React.FC = () =>  (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
         </Routes>  
     </BrowserRouter>
 );
